739e1d56714fbf7b1e858f10777870e489767bfd
Khuong Tran <khuongtran@khuongs-mbp.lan>
2022-01-09 20:52:39 -0800

    challenge

--- index.js
+++ index.js
@@ -72,5 +72,66 @@
 // radius 10
 // (2) ['radius', 'draw']
 
+// function Circle(radius) {
+//     this.radius = radius;
+//     let defaultLocation = { x: 0, y: 0};
+//     let computeOptimumLocation = function (){ // define with let so this function become private
+//         // this is function we want to hide in Object
+//         // ... 
+//     }
+//     this.draw = function(){
+//         this.computeOptimumLocation(0.1) 
+//         // by call function in this we hide a true function from the view
+//         console.log('draw')
+//     }
+// }
 
+// function Circle(radius) {
+//     this.radius = radius;
+//     let defaultLocation = { x: 0, y: 0};
+//     this.getDefaultLocation = function(){
+//         return defaultLocation;
+//     }
+//     this.draw = function(){
+//         console.log('draw')
+//     }
+//     Object.defineProperty(this, 'defaultLocation', {
+//         get: function(){
+//             return defaultLocation;
+//         },
+//         set: function(value) {
+//             if(!value.x || value.y)
+//                 throw new Error('Invalid location')
+//                 defaultLocation = value;
+//         }
+//     })
+// }
 
+function Stopwatch(){
+    let startTime, endTime, running, duration = 0
+    this.start = function(){
+        if(running) {
+            throw new Error('Stopwatch has already start')
+        }
+        running = true
+        startTime = new Date()
+    }
+    this.stop = function(){
+        if(!running) {
+            throw new Error('Stopwatch has already end')
+        }
+        running = false
+        endTime = new Date()
+        const seconds = (endTime.getTime() - startTime.getTime())/1000;
+        duration += seconds
+    }
+    this.reset = function() {
+        startTime = null
+        endTime = null
+        running = false
+        duration = 0
+    }
+    Object.defineProperty(this, 'duration', {
+        get: function(){ return duration;}
+    })
+}
